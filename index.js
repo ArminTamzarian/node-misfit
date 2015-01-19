d8b63e01c7d48f50e7e174b192c6bef5b8ee38cd
Armin Tamzarian <tamzarian1989@gmail.com>
2015-01-19 15:33:49 -0600

    Token exchange.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'user strict';
 
 var querystring = require('querystring');
+var request = require('superagent');
 var util = require('util');
 
 var NodeMisfit = (function () {
     var MISFIT_CLOUD_BASE_URL = 'https://api.misfitwearables.com';
 
     var PATH_AUTH_AUTHORIZE = '/auth/dialog/authorize';
+    var PATH_AUTH_EXCHANGE_TOKEN = '/auth/tokens/exchange';
 
     var nodeMisfit = function (options) {
         options = options || {};
@@ -25,6 +27,25 @@ var NodeMisfit = (function () {
                 redirect_uri: setup.redirectUri,
                 scope: setup.scope
             }));
+        };
+
+        this.getAccessToken = function(code, callback) {
+            request
+                .post(util.format('%s%s', MISFIT_CLOUD_BASE_URL, PATH_AUTH_EXCHANGE_TOKEN))
+                .send({
+                    grant_type: 'authorization_code',
+                    code: code,
+                    redirect_uri: redirectUri,
+                    client_id: setup.clientId,
+                    client_secret: setup.clientSecret
+                })
+                .end(function (err, response) {
+                    if(err) {
+                        return callback(err);
+                    }
+
+                    callback(null, response);
+                });
         };
     };
 
